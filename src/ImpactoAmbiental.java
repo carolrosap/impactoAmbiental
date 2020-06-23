8625bd69a41ca6ec2228bac814c923e766b101c3
Caroline Rosa <cpauladarosa@gmail.com>
2020-06-23 17:21:39 -0300

    linhas impacto

--- src/ImpactoAmbiental.java
+++ src/ImpactoAmbiental.java
@@ -1,31 +1,22 @@
 public class ImpactoAmbiental implements ImpactoCO2 {
 
-	
 	private double carbonoPorUnidade;
-
 	private double aguaUtilizadaPorUnidade;
-
 	private double areaDesmatadaPorUnidade;
-
 	private double impactoTotal;
-
 
 	public double calculaMediaCarbono(double nivelCo2, double nivelCo) {
 		return 0;
 	}
-
 	public double calculaMediaAgua(double quantidadeTotal, double unidades) {
 		return 0;
 	}
-
 	public double calculaAreaDesmatada(double quantidadeMadeira, String tipo) {
 		return 0;
 	}
-
 	@Override
 	public double calculaImpacto(Produto produto) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
